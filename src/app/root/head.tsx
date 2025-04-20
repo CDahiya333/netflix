d891bb25604948af6636b98794f207ddd7142e09
Chirag Dahiya <122281803+CDahiya333@users.noreply.github.com>
2025-04-21 04:10:16 +0530

    Update head.tsx

--- src/app/root/head.tsx
+++ src/app/root/head.tsx
@@ -1,12 +1,18 @@
 export default function Head() {
   return (
     <>
-      <title>Netflix - Watch TV Shows & Movies</title>
-      <meta name="description" content="Watch Netflix movies & TV shows online or stream right to your smart TV, game console, PC, Mac, mobile, tablet and more." />
+      <title>Netflix - Watch TV Shows, Series & Movies</title>
+      <meta
+        name="description"
+        content="Watch Netflix movies & TV shows online or stream right to your smart TV, game console, PC, Mac, mobile, tablet and more."
+      />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
       <meta property="og:title" content="Netflix - Watch TV Shows & Movies" />
-      <meta property="og:description" content="Watch Netflix movies & TV shows online or stream right to your smart TV, game console, PC, Mac, mobile, tablet and more." />
+      <meta
+        property="og:description"
+        content="Watch Netflix movies & TV shows online or stream right to your smart TV, game console, PC, Mac, mobile, tablet and more."
+      />
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content="Netflix" />
       {/* Browser configuration */}
